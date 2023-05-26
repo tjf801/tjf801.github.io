ec2856b04f06bc6a598d0fe5875e86329dc87750
tjf801 <fuscofamily0@gmail.com>
2023-05-25 23:12:59 -0400

    added splash text

--- nav.js
+++ nav.js
@@ -11,9 +11,36 @@ fetch('/nav.html')
             /href=\"\/?([0-9\-\.\?=a-zA-Z][0-9\/\-\.\?=a-zA-Z]*)\"/g,
             "href=\""+ '../'.repeat(layers) + (layers?'':'/') + "$1\""
         );
-        console.log(newelem.innerHTML);
+        // console.log(newelem.innerHTML);
         if (oldelem.parentNode) {
             oldelem.parentNode.replaceChild(newelem, oldelem);
         }
     }
+});
+
+fetch('/splashes.txt')
+.then(res => res.text())
+.then(file => {
+    const lines = file.split(/\n/);
+    
+    // lol this is such a dumb way to do this why does JS not have a random.seed
+    const hash = function(string) {
+        let result = 0;
+        if (string.length === 0) return result;
+        for (let i = 0; i < string.length; ++i) {
+            result = ((result << 5) - result) + string.charCodeAt(i);
+            result |= 0; // keep result as an integer ._.
+        }
+        return result;
+    };
+    
+    // Math.random() * lines.length
+    const salt = Math.floor(Date.now() / (1000 * 60 * 60 * 24));
+    const idx = Math.abs(hash(file + salt.toString())) % lines.length;
+    const choice = lines[idx];
+    
+    const splash = document.getElementById("splash-text");
+    if (!splash) return;
+    
+    splash.innerHTML = choice;
 })
